8141264ac755bf9745136228411dd03b805cb6fb
vscorza0093 <vinicius.scorza@hotmail.com>
2023-08-21 14:29:32 -0300

    Aula 01 - Lógica de programação com Dart

---
+++ aula01/dartapp/bin/dartapp.dart
@@ -0,0 +1,5 @@
+import 'package:dartapp/dartapp.dart' as dartapp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartapp.calculate()}!');
+}
---
+++ aula01/dartapp/lib/dartapp.dart
@@ -0,0 +1,13 @@
+int calculate() {
+  dynamic var1 = DateTime(2023, 08, 21);
+
+  List<String> txt = [];
+  print(var1);
+
+  var1 = [10, true, "A"];
+  print(var1);
+  var1.add("Dart", 100.000);
+  print(var1);
+
+  return 6 * 7;
+}
