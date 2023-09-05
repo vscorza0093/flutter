939222bd1493a022efd85465d5020914924908fb
vscorza0093 <vinicius.scorza@hotmail.com>
2023-09-05 17:55:12 -0300

    Aula 02 - Componentes Flutter

---
+++ aula02/componentesflutter/android/app/src/main/kotlin/com/viniciusscorza/flutterdiosantander/componentesflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.viniciusscorza.flutterdiosantander.componentesflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ aula02/componentesflutter/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Meu App"),
+      ),
+    ); // Scaffold
+  }
+}
