3bfdf139129aa711cbef33ba62bf8393cab5c9b4
vscorza0093 <vinicius.scorza@hotmail.com>
2023-09-11 12:57:37 -0300

    Aula 02 - Componentes Flutter e Tela de Login

--- aula02/componentesflutter/lib/pages/login_page.dart
+++ aula02/componentesflutter/lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +11,92 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 250, 196, 0),
+          body: SizedBox(
+            width: double.infinity,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Container(
+                    width: 150,
+                    height: 150,
+                    color: Colors.red,
+                    child: const Icon(
+                      Icons.person,
+                      size: 150,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    width: 400,
+                    height: 35,
+                    color: const Color(0xffEEEEEE),
+                    child: Text(
+                      "Informe seu email",
+                      style: GoogleFonts.abel(
+                          fontSize: 16,
+                          color: const Color.fromARGB(255, 104, 104, 104)),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: 400,
+                    height: 35,
+                    color: const Color(0xffEEEEEE),
+                    child: Text(
+                      "Informe sua senha",
+                      style: GoogleFonts.abel(
+                          fontSize: 16,
+                          color: const Color.fromARGB(255, 104, 104, 104)),
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  Container(
+                    padding: const EdgeInsets.all(15.0),
+                    width: 190,
+                    height: 60,
+                    color: const Color(0xffEEEEEE),
+                    child: Text(
+                      "Acessar",
+                      style: GoogleFonts.abel(fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  Container(
+                    padding: const EdgeInsets.all(15.0),
+                    width: 190,
+                    height: 60,
+                    color: const Color(0xffEEEEEE),
+                    child: Text(
+                      "Nova conta",
+                      style: GoogleFonts.abel(fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  Container(
+                      child: Text(
+                    "Direitos reservados",
+                    style: GoogleFonts.roboto(fontSize: 16),
+                  )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ]),
+          )),
+    );
   }
 }
---
+++ aula02/teladelogin/android/app/src/main/kotlin/com/viniciusscorza/teladelogin/teladelogin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.viniciusscorza.teladelogin.teladelogin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
