f64ad234372371e9cadead71c395a1439ccc930b
vscorza0093 <vinicius.scorza@hotmail.com>
2023-11-08 14:58:47 -0300

    Update Shopping App

--- balta-io-exercises/shopping/lib/pages/home.page.dart
+++ balta-io-exercises/shopping/lib/pages/home.page.dart
@@ -6,9 +6,6 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
-      ),
       body: Container(
         padding: const EdgeInsets.all(15),
         width: double.infinity,
@@ -17,9 +14,57 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             const SizedBox(
-              height: 60,
+              height: 50,
             ),
             search(),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "Categories",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 90,
+              child: categoryList(),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Best Selling",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                SizedBox(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "See All",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 300,
+              child: productList(),
+            )
           ],
         ),
       ),
@@ -29,7 +74,7 @@ class HomePage extends StatelessWidget {
 
 Widget search() {
   return Container(
-    height: 60,
+    height: 50,
     padding: const EdgeInsets.only(
       left: 20,
     ),
@@ -59,6 +104,117 @@ Widget search() {
               fontSize: 20,
               color: Colors.black,
             ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget categoryList() {
+  return SizedBox(
+    width: 70,
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+      ],
+    ),
+  );
+}
+
+Widget categoryItem() {
+  return Container(
+      width: 70,
+      height: 70,
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(1, 1),
+            blurRadius: 5,
+            spreadRadius: 2,
+          ),
+        ],
+        borderRadius: BorderRadius.all(
+          Radius.circular(64),
+        ),
+      ),
+      child: Image.asset("assets/Icon_Devices.png"));
+}
+
+Widget productList() {
+  return SizedBox(
+    width: 70,
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+      ],
+    ),
+  );
+}
+
+Widget productItem() {
+  return Container(
+    padding: const EdgeInsets.all(10),
+    margin: const EdgeInsets.all(5),
+    width: 170,
+    color: Colors.black12,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(
+          "assets/product-1.png",
+          width: 150,
+          height: 150,
+          fit: BoxFit.cover,
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const SizedBox(
+          height: 40,
+          child: Text(
+            "Titulo do produto",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ),
+        const Text(
+          "Marca",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        const Text(
+          "\$ 200",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF00C569),
           ),
         ),
       ],
